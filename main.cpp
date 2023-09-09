a54f1ea9bfbaac7020764eef2d094e5924645400
black <83869666+black-c-c@users.noreply.github.com>
2023-09-09 15:31:18 +0900

    add: メインループ内の処理を追加 #9

--- main.cpp
+++ main.cpp
@@ -5,9 +5,13 @@
 #include "mbed.h"
 #include "spirit.h"
 
+static constexpr float default_kp = 0.30f;
+static constexpr float default_ki = 0.80f;
+
 static constexpr uint32_t pc_baud   = 11'5200;
 static constexpr auto     loop_rate = 100ms;
 static constexpr uint32_t dip_sw    = 0x00;
+static constexpr float    max_rps   = 3.0f;
 
 /**
  * @brief モーターのデータをCANに書き込む
@@ -53,6 +57,8 @@ int main()
 
     spirit::Motor motor;
     motor.control_system(spirit::Motor::ControlSystem::PWM);
+    motor.pid_gain_factor(default_kp, default_ki, default_ki / 4.0f);
+
     motor.state(spirit::Motor::State::Brake);
     motor.duty_cycle(0.00F);
 
@@ -61,7 +67,88 @@ int main()
     BufferedSerial pc(USBTX, USBRX, pc_baud);
 
     while (true) {
-        // TODO PCから操作して逐次データを送信
+        if (pc.readable()) {
+            char pc_data;
+            pc.read(&pc_data, 1);
+            if (pc_data >= '0' && pc_data <= '9') {
+                if (motor.get_control_system() == spirit::Motor::ControlSystem::PWM) {
+                    motor.duty_cycle((pc_data - '0') * 0.10f);
+                    debug("duty : 0.%d0\r\n", (pc_data - '0'));
+                } else if (motor.get_control_system() == spirit::Motor::ControlSystem::Speed) {
+                    float rps = (pc_data - '0') / (9.0f / max_rps);
+                    motor.speed(rps);
+                    debug("rps  : %d.%d\r\n", (int)rps, (int)((rps - (int)rps) * 100));
+                } else {
+                    debug("control system error\r\n");
+                }
+            } else {
+                switch (pc_data) {
+                    case 'q':
+                        state = spirit::Motor::State::Coast;
+                        debug("state : Coast\r\n");
+                        break;
+                    case 'w':
+                        state = spirit::Motor::State::CW;
+                        debug("state : CW\r\n");
+                        break;
+                    case 'e':
+                        state = spirit::Motor::State::CCW;
+                        debug("state : CCW\r\n");
+                        break;
+                    case 'r':
+                        state = spirit::Motor::State::Brake;
+                        debug("state : Brake\r\n");
+                        break;
+                    case 'd':
+                        motor.control_system(spirit::Motor::ControlSystem::PWM);
+                        state = spirit::Motor::State::Brake;
+                        motor.duty_cycle(0.00F);
+                        debug("PWM Mode\r\n");
+                        break;
+                    case 's':
+                        motor.control_system(spirit::Motor::ControlSystem::Speed);
+                        state = spirit::Motor::State::Brake;
+                        motor.speed(0.00f);
+                        debug("Speed Control Mode\r\n");
+                        break;
+                    case 'g':
+                        if (motor.get_control_system() == spirit::Motor::ControlSystem::PWM) {
+                            debug("PWM Mode (No gain settings required)\r\n");
+                            break;
+                        }
+
+                        float kp, ki;
+
+                        motor.state(spirit::Motor::State::Brake);
+                        write(can, motor);
+
+                        debug("Gain Change Mode\r\nKp : ");
+                        
+                        if (scanf("%f", &kp) != 1) {
+                            kp = default_kp;
+                            debug("scan error [kp = default_kp]\r\n");
+                        }
+
+                        debug("Ki : ");
+
+                        if (scanf("%f", &ki)) {
+                            ki = default_ki;
+                            debug("scan error [ki = default_ki]\r\n");
+                        }
+
+                        motor.pid_gain_factor(kp, ki, ki / 4.0f);
+
+                        debug("Speed Control Mode\r\n");
+
+                        break;
+                    default:
+                        state = spirit::Motor::State::Brake;
+                        debug("***Input Error: Change state to Brake.***\r\n");
+                        break;
+                }
+                motor.state(state);
+            }
+        }
 
         write(can, motor);
         ThisThread::sleep_for(loop_rate);
